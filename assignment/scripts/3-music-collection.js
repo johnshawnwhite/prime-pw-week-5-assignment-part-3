3762df7383ca4c60b2bb8df5061257a445a2a4eb
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-06-03 18:00:37 -0400

    mando done

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -19,6 +19,17 @@ function addtocollection (title, artist, yearPublished){
    return album;
 } // End addtocollection
 
+// let numbers =[];
+// function addition(x,y){
+//   const numenter = {
+//    firstnum: x,
+//    secondnum: y
+//  }
+//  numbers.push(numenter);
+// }
+// addition(1,2);
+// console.log(numbers);
+
 
 //- Test the `addToCollection` function:
 //  - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
@@ -26,15 +37,10 @@ function addtocollection (title, artist, yearPublished){
 //  - After all are added, console.log the `collection` array.
 
 addtocollection('Rock', 'Boomer', '1945');
-console.log(collection);
-addtocollection('Rap', 'Aysia', '2005');
-console.log(collection);
+addtocollection('Rap', 'Luke', '2005');
 addtocollection('Country', 'Xman', '2017');
-console.log(collection);
 addtocollection('Smooth Jazz', 'Luke', '1987');
-console.log(collection);
 addtocollection('Boom Boom', 'Mary', '1999');
-console.log(collection);
 addtocollection('Noises', 'Madison', '2001');
 console.log(collection);
 
@@ -52,13 +58,40 @@ function showCollection (collection) {
 }
 console.log(collection.length);
 //- Test the `showCollection` function.
-
+showCollection(collection);
 //- Add a function named `findByArtist`. This function should:
-//  - Take in `artist` (a string) parameter
+//  - Take in `artist` (a string) parameter//
 //  - Create an array to hold any results, empty to start
 //  - Loop through the `collection` and add any objects with a matching artist to the array.
 //  - Return the array with the matching results. If no results are found, return an empty array.
 
+function findByArtist(artist){
+  let artistmatch = [];
+  console.log(`Looking through collection for ${artist}.`);
+  for (i = 0; i<collection.length; i++) {
+    if (collection[i].artist === artist){
+      artistmatch.push(collection[i]);
+}
+}
+return artistmatch;
+}
+findByArtist('Luke');
+console.log(findByArtist('Luke'));
+showCollection(findByArtist('Luke'));
+// function findByArtist(artist){
+//   let artistmatch = [];
+//   console.log(`Looking through collection for ${artist}.`);
+//   for (i = 0; i<collection.length; i++) {
+//     if (collection[i].artist === artist){
+//     console.log('You have a match!!!');
+//     artistmatch.push(collection[i]);
+//     // adds the artist to array of artist matches
+//   } else {
+//     console.log(`${artist} does not match ${collection[i].artist}`);
+//   } // returns the search negative
+//  }
+//  return artistmatch;
+// }
 //- Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
 
 //> When testing your functions, write all tests in the JavaScript file!
